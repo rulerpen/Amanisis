3eff9230d929b2db921e3f644a36778c9590b938
rulerpen <2211477938@qq.com>
2021-11-18 20:23:22 +0800

    add interface

--- balance/common.go
+++ balance/common.go
@@ -1,5 +1,7 @@
 package balance
 
-func GetServer(list []interface{}, mode int) (num int) {
+import "Amanisis/model/registerModel"
+
+func GetServer(list []registerModel.Register, mode int) (num int) {
 	return
 }
