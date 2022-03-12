f0e6aebbe348a2d8a82c1e63effe279eae3f309e
SidddMeier <qzb200011@163.com>
2022-03-12 21:12:19 +0800

    qzb

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"Amanisis/register"
+	"fmt"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"sync"
 )
 
-func main(){
+func main() {
 
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go register.Server(wg.Done)
+	fmt.Printf("123")
 	wg.Wait()
 
 }
